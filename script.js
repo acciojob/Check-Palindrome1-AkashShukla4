e395960368e8526484965e4d569d0a26c074ca87
AkashShukla4 <117896950+AkashShukla4@users.noreply.github.com>
2023-03-26 16:40:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-
+str.join(" ");
+const n = str.length;
+	var a= n;
+	for(let i = 0; i<n; i++){
+		if(str.charAt(i) == str.charAt(n));
+		else{
+			return false;
+		}
+	}
+	return true;
 }
 module.exports = palindrome
