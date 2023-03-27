37830eaf794b2507f4e120429d9b35d41a13f43d
AkashShukla4 <117896950+AkashShukla4@users.noreply.github.com>
2023-03-27 15:04:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ str.join(" ");
 const n = str.length;
 	var a= n;
 	for(let i = 0; i<n; i++){
-		if(str.charAt(i) == str.charAt(n));
-		else{
+		if(str.charAt(i) != str.charAt(n)){
 			return false;
 		}
 		n--;
